704589ceec9195f601b1f5236a8da54d522ebf1f
Dylan Mora <dylanmora@ufl.edu>
2022-12-03 12:52:35 -0500

    Book.h revised

--- Book.h
+++ Book.h
@@ -2,37 +2,29 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include "User.h"
-#include "Reviews.h"
-using namespace std;
+#include <map>
 
-//TODO: ADD MORE IF YOU CAN OF ANYTHING ELSE WE MAY NEED
-//Leaving Book class as public. If you think private is better lets discuss with the team.
+//#include "Reviews.h"
+using namespace std;
 
 struct Book{
     string isbn;
     string author;
     string year;
     bool availability;
-   
-    string reviews;
-    vector<Reviews> review_history;
+    //vector<Book> bookCollection;
+    string rating;
+    vector<string> bookRatings;
+     
+    // ISBN     BOOK OBJECT ratings 
+    map<string, Book> bookCollection;
 
     Book();
-    Book(string isbn);
-    
+    Book(string _isbn, string _author, string _year, bool _availability, string _rating, vector<string> _bookRatings);
 
-    //When the user turns in a book prompt the option to add a review
-    void add_review(string username);
-    //Delete review if the user decided they don't want to keep it.
-    void delete_review(Reviews review);
-    //Print review History
-    void print_reviews(vector<Reviews> review_history);
-    //Print reviews based on filtered rating
-    void print_filteredReviews(vector<Reviews> review_history, int rating);
-    //addBook() database.
-    //removeBooks database
-
+    void instantiateBookCollection();
+    void add_review();
+    void print_ratings(string isbn);
     static bool searchBooks(string isbn);
 };
 
@@ -40,95 +32,110 @@ Book::Book() {
     author = "";
     this->isbn = "-1";
     availability = false;
-    year = "";
-    reviews = "";
+    year = ""; 
+}
+Book::Book(string _isbn, string _author, string _year, bool _availability, string _rating, vector<string> _bookRatings) {
+    isbn = _isbn;
+    author = _author;
+    availability = _availability;
+    year = _year;
+    rating = _rating;
+
+    for (int i = 0; i < _bookRatings.size(); i++) {
+        bookRatings.push_back(bookRatings[i]);
+    }
 }
 
-Book::Book(string _isbn) {
+//Since we are preStoring Books onto the database...
+//ALL BOOKS NEED TO BE STORED IN THE VECTOR. READ THE DATABASE AND CREATE BOOK OBJECTS
+void Book::instantiateBookCollection() {
     fstream file("Database.json");		//Loads the file stream
     Json::Reader reader;				//Read and parse JSON data
     //Json::StyledStreamWriter writer;	//Write JSON data in human-readable form
     Json::Value data;					//Store JSON data and make it accessible
-
     reader.parse(file, data);			//Read a Value from a JSON document. 
 
-    isbn = _isbn;                                     //Assign Book an ISBN number
-    author = data["books"][isbn]["author"].asString();//Assign author variable with name from database
-    year = data["books"][isbn]["year"].asString();    //Assign year variable with year from database
-    availability = data["books"][isbn]["available"].asBool(); //Assign availability variable with status from database
+    //Store all books from the "JSON database" into a vector for easier access. 
+    for (auto const& id : data["books"].getMemberNames()) {
 
-    //reviews = "PULL REVIEWS FROM DATABASE";
-    //review_history is empty till we add a review.
+        if (id != "-1") {
+            isbn = id;                      //Assign Book an ISBN number
+            author = data["books"][id]["author"].asString();          //Assign author variable with name from database
+            year = data["books"][id]["year"].asString();              //Assign year variable with year from database
+            availability = data["books"][id]["available"].asBool();   //Assign availability variable with status from database
+            rating = data["books"][id]["rating"].asString();
+
+            for (int j = 0; j < data["books"][id]["Ratings"].size(); j++) {
+                bookRatings.push_back(data["books"][id]["Ratings"][j].asString());
+            }
+
+
+
+            Book bookFromJSON(isbn, author, year, availability, rating, bookRatings); //Create object
+            bookCollection.insert(pair<string, Book>(isbn, bookFromJSON));           //Store isbn and book into map
+        }
+    }
 }
 
 
 //add review parameter
-void Book::add_review(string username) {
+void Book::add_review() {
     fstream file("Database.json");		//Loads the file stream
     Json::Reader reader;				//Read and parse JSON data
     Json::StyledStreamWriter writer;	//Write JSON data in human-readable form
     Json::Value data;					//Store JSON data and make it accessible
-
     reader.parse(file, data);			//Read a Value from a JSON document. 
 
-    cout << "Here at Books Without Boundaries, leaving reviews is very important to our success. We'd love to get your feedback on the current book that you have read." << endl;
-    cout << "Enter (Y/N) to leave a review." << endl;
-    char leave_review;
-    cin >> leave_review;
+    //Utilizing SMFL, Ask user if they want to leave a review...
+    //Click on the star 1 - 5. Each star should have corresponding rating...
+    //Grab the rating the User has selected and store that into the database
+    //For now we use terminal as an example.
+    cout << "Enter a rating from 1-5 for the returning book: " << endl;
+    cin >> rating;
+    //Add book rating to the database.... Store into JSON
+    data["books"][isbn]["ratings"].append(rating);
 
-    if (leave_review == 'Y' || 'y') {
-        string reviewerName;
-        string rating;
-        string feedback;
 
-        cout << "Name: " << endl;
-        cin >> reviewerName;
-        cout << "Enter a rating from 1-5 for the returning book: " << endl;
-        cin >> rating;
-        cout << "Tell us how the book was:  " << endl;
-        cin >> feedback;
-
-        //Reviews reviewObject(reviewerName, rating, feedback);
-        //*NOTES* create map for REVIEWS
-
-        //Assuming that we have a list of books to checkout from. Write the review to that corresponding book
-        //* NOTES * since this is being written to the database, do we not have to create an object?
-
-        data["books"][isbn]["reviewName"].append(reviewerName);
-        data["books"][isbn]["rating"].append(rating);
-        data["books"][isbn]["feedback"].append(feedback);
-
-        cout << "We're so happy to hear from you. Thank you for providing us with valuable feedback!" << endl;
-
-    }
-    else {
-        cout << "Thank you for your return. Have a nice day!";
+    //UPDATE RATINGS of the current book------------------------------------------------------------ 
+    //Access the current book ratings
+    int average;
+    int calculation;
+    for (int i = 0; i < data["books"][isbn]["ratings"].size(); i++) {
+        average += data["books"][isbn]["ratings"][i].asInt();
     }
 
+    calculation = average / data["books"][isbn]["ratings"].size();  //INT
+    rating = to_string(calculation);                                //Convert into string
+    data["books"][isbn]["rating"] = rating;                         //Store into JSON      
+    
 
-    //Writes to the database.
     file.close();
     ofstream closer;
     closer.open("Database.json");
     closer.close();
     file.open("Database.json");
-
     writer.write(file, data);
+}
+void Book::print_ratings(string isbn) {
+    fstream file("Database.json");		//Loads the file stream
+    Json::Reader reader;				//Read and parse JSON data
+    Json::StyledStreamWriter writer;	//Write JSON data in human-readable form
+    Json::Value data;					//Store JSON data and make it accessible
+    reader.parse(file, data);			//Read a Value from a JSON document. 
 
-    //Not needed since we store in the database?
-    //Push into review container.
-    //review_history.push_back(Reviews);
-}
-void Book::delete_review(Reviews review) {
-    //TODO: Iterate through review history and delete the selected review based on parameters given
-}
-void Book::print_reviews(vector<Reviews> review_history) {
-    //TODO: Print all reviews with name, rating, and the feedback.
-//    for (int i = 0; i < review_history.size; i++) {
-//    }
-}
-void Book::print_filteredReviews(vector<Reviews> review_history, int rating) {
-    //TODO: Print all review from selected rating.  with name and the feedback.
+    /*for (int i = 0; i < data["books"][isbn]["ratings"].size(); i++) {
+    }*/
+
+    //PRINTS ALL RATINGS.
+    for (auto it = bookCollection.begin(); it != bookCollection.end(); ++it){
+
+        if (it->first == isbn) {
+
+            for (int i = 0; i < it->second.bookRatings.size(); i++) {
+                cout << it->second.bookRatings[i];
+            }
+        }
+    }
 }
 
 bool Book::searchBooks(string isbn){
