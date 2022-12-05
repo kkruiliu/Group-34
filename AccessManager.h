43255c7cc7d2f421464c54f8722eddee86d0c1d2
Joshua Fine <joshfine2003@gmail.com>
2022-12-04 22:51:20 -0500

    Updated User, main, and AccessManager to all use database instead of local vectors

--- AccessManager.h
+++ AccessManager.h
@@ -14,93 +14,47 @@ ifstream fin("User.json");
 ofstream fout("User.json", ios_base::app);
 
 class AccessManager {
-
-private:
-    vector <string> usernames; //all the usernames of the existing accounts
-    vector <string> passwords; //all the passwords of the existing accounts
-    vector <string> id_;
-
 public:
+    User activeUser;
     bool loggedIn = false;
 
     bool SignUp(string username, string password) {
-        //TODO: Randomly Generate ID
-  
         //Accepts upper case/ lower case/ underscores / numbers. 6-16 characters
         regex usernameRequirement = regex("^[A-Za-z0-9_]{6,16}$");
-        bool  validUsername = regex_search(username,usernameRequirement);
         //Minimum 6 and maximum 16 characters, at least one uppercase letter, one lowercase letter,
         // one number and one special character
         regex passwordRequirement = regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,16}$");
-        bool  validPassword = regex_search(password,passwordRequirement);
-        if (!validUsername) {
+        if (!regex_search(username, usernameRequirement) || !regex_search(password, passwordRequirement)) {
             return false;
         }
-        if (!validPassword) {
-            return false;
-        }
-        if (validUsername && validPassword) {
-            insert_username(username);
-            insert_password(password);
-
-            User currentUser(username, password, "Default Name");
-
-            return true;
-        }
-        
-
-        //TODO: After Creating account. Need to take in profile details such as name.
-        //TODO: Create accountDetails() function
-        //AccountDetails();
-
-        //Sets the given username and passwords as the eUsername and ePassword
-        //fout << username << " " << password << " " << id << "\n";
-    }
-    void AccountDetails (){
-        //This will take in the name of the user to create the user object associated with that username and password.
-
+        activeUser = User(username, password);
+        return true;
     }
 
 
-    void LoginIn(string username, string password) {
-        //create_vectors();
-        int size = accounts_number(); //the number of the accounts
-
-        //searches for an account that has the username, that the user entered
-        for (int i = 0; i < size; i++) {
-            if ((username == usernames[i]) && (password == passwords[i])) {
+    void logIn(string username, string password) {
+        Json::Value data = readData();
+        bool validUser = false;
+        for (auto const& user : data["users"].getMemberNames()) {
+            if (username == user)
+                validUser = true;
+        }
+        if (validUser) {
+            if (data["users"][username]["password"].asString() == password) {
                 loggedIn = true;
+                activeUser = User(username);
             }
-        }   
+        }
     }
 
-    //creates the usernames and the passwords vectors
-    void create_vectors() {
-        string username, password, id;
-        while (fin >> username && fin >> password && fin >> id) {
-            insert_username(username); //insert the username read from the file into the vector
-            insert_password(password);
-            insert_id(id);
-        }
+    void logOut() {
+        activeUser = User();
+        loggedIn = false;
     }
 
     //returns the numbers of the accountts in the file
     int accounts_number() {
-        return usernames.size();
-    }
-
-    //inserts a new element in the usernames vector
-    void insert_username(string username) {
-        usernames.push_back(username);
-    }
-
-    //inserts a new element in the passwords vector
-    void insert_password(string password) {
-        passwords.push_back(password);
-    }
-
-    void insert_id(string id) {
-        id_.push_back(id);
+        return readData().getMemberNames().size();
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -180,7 +180,7 @@ void LoadCreateAccWindow(sf::RenderWindow& window, sf::RectangleShape& usernameB
 
 }
 
-void LoadLoginWindow(sf::RenderWindow& window, string username) {
+void LoadLoginWindow(sf::RenderWindow& window, AccessManager& control) {
     setTitle(window);
 
     sf::Font font = loadFont("Montserrat-Bold.ttf");
@@ -190,7 +190,7 @@ void LoadLoginWindow(sf::RenderWindow& window, string username) {
     welcomeText.setCharacterSize(45);
     welcomeText.setPosition(sf::Vector2f(10, 200));
     welcomeText.setFillColor(sf::Color::Blue);
-    welcomeText.setString("Welcome, " + username);
+    welcomeText.setString("Welcome, " + control.activeUser.getUsername());
 
     window.draw(welcomeText);
 }
@@ -231,7 +231,7 @@ bool VerifyLogin(sf::RenderWindow& window, AccessManager& control, textBox& user
     string user = username.getText();
     string pass = password.getText();
 
-    control.LoginIn(user, pass);
+    control.logIn(user, pass);
 
     return control.loggedIn;
 }
@@ -554,29 +554,12 @@ int main(int argc, char const** argv){
         }
 
         if (loginWinOpen) {
-            string username = userText.getText();
-            LoadLoginWindow(window, username);
+            LoadLoginWindow(window, control);
             signOut.drawTo(window);
         }
 
         window.display();
     }
-
-    //AcessManager control;
-
-    //control.SignUp();
-    //control.LoginIn();
-
-    User user1("john224");
-    cout << "(1) INITIAL STATE" << endl;
-    user1.print();
-    user1.returnBook();
-    cout << "(2) BOOK RETURN" << endl;
-    user1.print();
-    user1.checkoutBook("502");
-    cout << "(3) BOOK CHECKOUT" << endl;
-    user1.print();
-
 
     return 0;
 }
